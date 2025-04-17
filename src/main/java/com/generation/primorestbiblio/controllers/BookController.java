f9fffd3d1dd0ede70a12c385e73c2677337dcf39
StefanoR <rubinetti.stefano@gmail.com>
2025-04-17 17:52:36 +0200

    aa

--- src/main/java/com/generation/primorestbiblio/controllers/BookController.java
+++ src/main/java/com/generation/primorestbiblio/controllers/BookController.java
@@ -3,15 +3,13 @@ package com.generation.primorestbiblio.controllers;
 import com.generation.primorestbiblio.model.dao.BookDao;
 import com.generation.primorestbiblio.model.entities.Book;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 //Lui crea un controller per lo scambio di dati
 @RestController
+@RequestMapping("/api")
 public class BookController
 {
 	@Autowired
